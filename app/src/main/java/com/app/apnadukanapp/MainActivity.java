5505f7c00bea1f444c757fe8896743d23431991a
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-10-31 11:08:07 +0530

    FK

---
+++ app/src/main/java/com/app/apnadukanapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.app.apnadukanapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.apnadukanapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
